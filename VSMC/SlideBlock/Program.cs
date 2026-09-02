007eb15ef13536638575946cf8d64174402925b0
astratom <astraltom07@gmail.com>
2026-09-02 14:55:08 +0900

    게임 프로젝트 세팅(완)

--- VSMC/SlideBlock/Program.cs
+++ VSMC/SlideBlock/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace SlideBlock
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace SlideBlock
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
